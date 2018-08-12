ca41db772b36a8f409a3966c010cf3edb98fd011
NuriSEZGİN <nnurisezgin@gmail.com>
2018-08-12 22:48:34 +0300

    Added extra util methods to ViewResources

--- app/src/main/java/nurisezgin/com/mine/OptionalResources.java
+++ app/src/main/java/nurisezgin/com/mine/OptionalResources.java
@@ -13,41 +13,43 @@ import android.support.v4.content.ContextCompat;
 
 import polanski.option.Option;
 
+import static polanski.option.Option.*;
+
 public final class OptionalResources {
 
     public static Option<String> getString(Context context, @StringRes int id) {
-        return Option.tryAsOption(() -> context.getString(id));
+        return tryAsOption(() -> context.getString(id));
     }
 
     public static Option<String> getString(Context context, @StringRes int id, String... args) {
-        return Option.tryAsOption(() -> context.getString(id, args));
+        return tryAsOption(() -> context.getString(id, args));
     }
 
     public static Option<Integer> getInteger(Context context, @IntegerRes int id) {
-        return Option.tryAsOption(() -> context.getResources().getInteger(id));
+        return tryAsOption(() -> context.getResources().getInteger(id));
     }
 
     public static Option<Boolean> getBoolean(Context context, @BoolRes int id) {
-        return Option.tryAsOption(() -> context.getResources().getBoolean(id));
+        return tryAsOption(() -> context.getResources().getBoolean(id));
     }
 
     public static Option<Integer> getDimension(Context context, @DimenRes int id) {
-        return Option.tryAsOption(() -> context.getResources().getDimensionPixelSize(id));
+        return tryAsOption(() -> context.getResources().getDimensionPixelSize(id));
     }
 
     public static Option<String[]> getStrings(Context context, @ArrayRes int id) {
-        return Option.tryAsOption(() -> context.getResources().getStringArray(id));
+        return tryAsOption(() -> context.getResources().getStringArray(id));
     }
 
     public static Option<int[]> getIntegers(Context context, @ArrayRes int id) {
-        return Option.tryAsOption(() -> context.getResources().getIntArray(id));
+        return tryAsOption(() -> context.getResources().getIntArray(id));
     }
 
     public static Option<Integer> getColor(Context context, @ColorRes int id) {
-        return Option.tryAsOption(() -> ContextCompat.getColor(context, id));
+        return tryAsOption(() -> ContextCompat.getColor(context, id));
     }
 
     public static Option<Drawable> getDrawable(Context context, @DrawableRes int id) {
-        return Option.tryAsOption(() -> ContextCompat.getDrawable(context, id));
+        return tryAsOption(() -> ContextCompat.getDrawable(context, id));
     }
 }
--- app/src/main/java/nurisezgin/com/mine/ViewResources.java
+++ app/src/main/java/nurisezgin/com/mine/ViewResources.java
@@ -8,23 +8,58 @@ import android.support.annotation.ColorRes;
 import android.support.annotation.DimenRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Px;
-import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
+
+import static android.support.v4.content.ContextCompat.getColor;
+import static android.support.v4.content.ContextCompat.getDrawable;
+import static polanski.option.Option.tryAsOption;
 
 public final class ViewResources {
 
     public static void setBackgroundColor(View view, @ColorRes int id) {
         Context context = view.getContext();
-        @ColorInt int color = ContextCompat.getColor(context, id);
+        @ColorInt int color = getColor(context, id);
         view.setBackgroundColor(color);
+    }
+
+    public static void setBackgroundColorRes(View view, @ColorRes int id, @ColorInt int def) {
+        Context context = view.getContext();
+
+        view.setBackgroundColor(tryAsOption(() -> getColor(context, id))
+                .map(colorInt -> colorInt)
+                .orDefault(() -> def));
+    }
+
+    public static void setBackgroundColor(View view, @ColorRes int id, @ColorRes int def) {
+        Context context = view.getContext();
+
+        view.setBackgroundColor(tryAsOption(() -> getColor(context, id))
+                .map(colorInt -> colorInt)
+                .orDefault(() -> getColor(context, def)));
     }
 
     public static void setBackground(View view, @DrawableRes int id) {
         Context context = view.getContext();
-        Drawable drawable = ContextCompat.getDrawable(context, id);
+        Drawable drawable = getDrawable(context, id);
         setBackground(view, drawable);
+    }
+
+    public static void setBackground(View view, @DrawableRes int id, @DrawableRes int def) {
+        Context context = view.getContext();
+
+        setBackground(view, tryAsOption(() -> getDrawable(context, id))
+                .map(drawable -> drawable)
+                .orDefault(() -> getDrawable(context, def)));
+    }
+
+    public static void setBackground(View view, @DrawableRes int id, Drawable def) {
+        Context context = view.getContext();
+
+        setBackground(view, tryAsOption(() -> getDrawable(context, id))
+                .map(drawable -> drawable)
+                .orDefault(() -> def));
     }
 
     public static void setBackground(View view, Drawable drawable) {
@@ -35,15 +70,45 @@ public final class ViewResources {
         }
     }
 
-    public static void setTextColor(TextView view, @ColorRes int color) {
-        @ColorInt int textColor = ContextCompat.getColor(view.getContext(), color);
+    public static void setTextColor(TextView view, @ColorRes int id) {
+        @ColorInt int textColor = getColor(view.getContext(), id);
         view.setTextColor(textColor);
+    }
+
+    public static void setTextColorRes(TextView view, @ColorRes int id, @ColorRes int def) {
+        Context context = view.getContext();
+        view.setTextColor(tryAsOption(() -> getColor(context, id))
+                .map(colorInt -> colorInt)
+                .orDefault(() -> getColor(context, def)));
+    }
+
+    public static void setTextColor(TextView view, @ColorRes int id, @ColorInt int def) {
+        Context context = view.getContext();
+        view.setTextColor(tryAsOption(() -> getColor(context, id))
+                .map(colorInt -> colorInt)
+                .orDefault(() -> def));
     }
 
     public static void setTextSize(TextView view, @DimenRes int id) {
         Context context = view.getContext();
         float size = context.getResources().getDimension(id);
         view.setTextSize(TypedValue.COMPLEX_UNIT_SP, size);
+    }
+
+    public static void setTextSize(TextView view, @DimenRes int id, @DimenRes int def) {
+        Context context = view.getContext();
+        view.setTextSize(TypedValue.COMPLEX_UNIT_SP,
+                tryAsOption(() -> context.getResources().getDimension(id))
+                .map(size -> size)
+                .orDefault(() -> context.getResources().getDimension(def)));
+    }
+
+    public static void setTextSize(TextView view, @DimenRes int id, float def) {
+        Context context = view.getContext();
+        view.setTextSize(TypedValue.COMPLEX_UNIT_SP,
+                tryAsOption(() -> context.getResources().getDimension(id))
+                .map(size -> size)
+                .orDefault(() -> def));
     }
 
     public static void setTextSizeAsPixel(TextView view, @Px int size) {
